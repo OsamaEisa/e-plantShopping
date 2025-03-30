e996c454b1401ef5358cbc850e48266ea6a60b9f
OsamaEisa <engosama46@yahoo.com>
2025-03-30 13:45:24 -0400

    finishing the tasks-2

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -10,5 +10,34 @@ const cartSlice = createSlice({
     },
 });
 
+return (
+    <div className="cart-container">
+      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <div>
+        {cart.map(item => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-quantity">
+                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <span className="cart-item-quantity-value">{item.quantity}</span>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+              </div>
+              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
+              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="continue_shopping_btn">
+        <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
+        <br />
+        <button className="get-started-button1" onClick={() => alert('Functionality to be added for future reference')}>Checkout</button>
+      </div>
+    </div>
+  );
+
 export const { addItem } = cartSlice.actions;
 export default cartSlice.reducer;
